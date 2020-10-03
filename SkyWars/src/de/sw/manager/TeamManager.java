eabb3c23d61e66a038f477a0fce9a0d951d0f56f
PoxhYT <ernst.wedel@gmx.de>
2020-10-03 14:26:27 +0200

    Update TeamManager.java

--- SkyWars/src/de/sw/manager/TeamManager.java
+++ SkyWars/src/de/sw/manager/TeamManager.java
@@ -1,12 +1,29 @@
 package de.sw.manager;
 
+import org.bukkit.entity.Player;
+
 import java.util.List;
 
 public class TeamManager {
 
     private List<Team> teams;
+    private String teamName;
+    private int playersInTeam;
+    private List<Player> players;
 
-    public TeamManager(List<Team> teams) {
+    public TeamManager(List<Team> teams, String teamName, int playersInTeam, List<Player> players) {
         this.teams = teams;
-}
+        this.teamName = teamName;
+        this.playersInTeam = playersInTeam;
+        this.players = players;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
 }
