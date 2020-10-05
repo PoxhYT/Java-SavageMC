b11decfd396d05ef738e975e364a03e152222198
Solomon Rosemite™ <60587271+SolomonRosemite@users.noreply.github.com>
2020-10-05 12:42:11 +0200

    Added Config for Services

--- Services/src/de/services/main/Main.java
+++ Services/src/de/services/main/Main.java
@@ -1,10 +1,11 @@
 package de.services.main;
 
-import de.services.filemanager.FileManager;
+import de.services.config.Config;
 import de.services.mysql.MySQL;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
@@ -13,39 +14,30 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main extends JavaPlugin {
     public static String prefix = "§8[MySQL] §7";
-    public static Main instance;
 
     public MySQL mySQL;
 
-    private FileManager manager;
-    private final String path = "plugins/Services.json";
+    private Config config;
+
+    private String host, database, username, password;
+
+    private int port;
 
     public void onEnable() {
-        instance = this;
-        manager = new FileManager();
+        config = new Config(this);
 
-//        try {
-//            getData();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        loadConfiguration();
+        mySQL = new MySQL(host, database, username, password, port);
 
         // Connect to Database
-        mySQL = new MySQL("localhost", "savagemc", "root", "somePassword", 3306);
-        try {
-            mySQL.connect();
-            log("§eDas Plugin wurde erfolgreich gestartet!");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        connectMySQL();
     }
 
     public void onDisable() {
@@ -53,11 +45,24 @@ public class Main extends JavaPlugin {
             mySQL.disable();
     }
 
-//    private void getData() throws InterruptedException {
-//        System.out.println(manager.loadData(path));
-//    }
+    private void connectMySQL() {
+        try {
+            mySQL.connect();
+            log("§eMit MySQL verbunden!");
+        } catch (SQLException | ClassNotFoundException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public void loadConfiguration() {
+        final String sql = "mysql.credentials.";
+
+        host = (String) config.getConfiguration(sql+"host");
+        database = (String) config.getConfiguration(sql+"database");
+        username = (String) config.getConfiguration(sql+"username");
+        password = (String) config.getConfiguration(sql+"password");
+        port = (int) config.getConfiguration(sql+"port");
+    }
 
     private void log(Object message) { Bukkit.getConsoleSender().sendMessage(prefix + message); }
-
-    public Main getInstance() { return instance; }
 }
