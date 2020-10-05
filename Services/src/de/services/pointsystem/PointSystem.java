6295942e4aa865dc49de9268c8afcf0bee9d2723
Solomon Rosemite™ <60587271+SolomonRosemite@users.noreply.github.com>
2020-10-05 19:57:23 +0200

    Player SoupScore can now be Saved to MySQL

--- Services/src/de/services/pointsystem/PointSystem.java
+++ Services/src/de/services/pointsystem/PointSystem.java
@@ -5,20 +5,39 @@ import de.soup.events.SoupListener;
 import de.soup.storage.SpeedType;
 import org.bukkit.entity.Player;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
+
 public class PointSystem {
-    private MySQL sql;
+    private MySQL mySQL;
 
     public PointSystem(MySQL sql) {
-        this.sql = sql;
+        this.mySQL = sql;
     }
 
     public void saveForSoupScore(Player player, SpeedType type, int score) {
+        String sql = "INSERT INTO scores (uuid, soupScore) VALUES (?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = mySQL.getConnection().prepareStatement(sql);
+            preparedStatement.setString(1, player.getUniqueId().toString());
+            preparedStatement.setInt(2, score);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e)  {
+            e.getStackTrace();
+        }
         // TODO: Receive Score On gameOver or /soup stop command
         // TODO: Save for Player
     }
 
-    public void getScoresForSoup(Player player) {
-        // TODO: Search by player
+    public void getScoresForSoup(UUID id) throws SQLException {
+        // TODO: Search by UUID
+        String sql = "SELECT * FROM scores WHERE uuid=" + id.toString();
+
+        PreparedStatement preparedStatement = mySQL.getConnection().prepareStatement(sql);
+        preparedStatement.executeUpdate();
     }
 
     public void getScoresForSoup(SpeedType type) {
