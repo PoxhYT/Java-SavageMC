e43e7e4b2cef2e13e4f2ec337450374f9dff9d34
PoxhYT <ernst.wedel@gmx.de>
2020-11-07 11:25:17 +0100

    Map

--- SkyWars/src/de/sw/commands/Command_SkyWars.java
+++ SkyWars/src/de/sw/commands/Command_SkyWars.java
@@ -2,14 +2,20 @@ package de.sw.commands;
 
 
 import de.sw.api.LocationAPI;
+import de.sw.enums.Path;
 import de.sw.main.Main;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
+import java.io.File;
+
 public class Command_SkyWars implements CommandExecutor {
+    private static File fileSkywars = new File("plugins/SkyWars", "config.yml");
+    private static YamlConfiguration yamlConfigurationSkyWars = YamlConfiguration.loadConfiguration(fileSkywars);
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -19,18 +25,24 @@ public class Command_SkyWars implements CommandExecutor {
                 if (args.length == 0) {
                     player.sendMessage("§8➥ §f/sw setloc <name>");
                 } else {
-                    if(args.length == 1) {
-                        if (args[1].equals("setloc")) {
+                    if (args[1].equals("setloc")) {
+                        if (args.length == 1) {
                             player.sendMessage("§8➥ §f/sw setloc <name>");
                             player.playSound(player.getLocation(), Sound.VILLAGER_DEATH, 1, 1);
-                        }
-                    }
-
+                        } else
+                            player.sendMessage("§8➥ §f/sw setloc <name>");
                     if (args.length == 2) {
-                        if (args[1].equals("lobby")) {
-                            LocationAPI.setSpawn("Lobby", player.getLocation());
-                            player.sendMessage(Main.prefix + "§7Du hast die §eLobby §7erfolgreich gesetzt!");
-                            player.playSound(player.getLocation(), Sound.LEVEL_UP, 1, 1);
+                        try {
+                            int spawnNumber1 = Integer.parseInt(args[2]);
+                            if(spawnNumber1 > 0 && spawnNumber1 <= yamlConfigurationSkyWars.getInt("Settings.MaxPlayers")) {
+                                LocationAPI.setSpawn(args[2], player.getLocation());
+                                player.sendMessage(Main.prefix + "Du hast die §eLocation §7" + args[2] + " §7gesetzt!");
+                            } else
+                                player.sendMessage(Main.prefix + "Gebe eine Nummer zwischen 1 und " + yamlConfigurationSkyWars.getInt("Settings.MaxPlayers") + " ein!");
+                        }catch (NumberFormatException e){}
+                        LocationAPI.setSpawn("Lobby", player.getLocation());
+                        player.sendMessage(Main.prefix + "§7Du hast die §eLobby §7erfolgreich gesetzt!");
+                        player.playSound(player.getLocation(), Sound.LEVEL_UP, 1, 1);
                         }
                     }
                 }
--- SkyWars/src/de/sw/map/Map.java
+++ SkyWars/src/de/sw/map/Map.java
@@ -9,5 +9,8 @@ import java.io.File;
 
 public class Map {
     private Location[] spawnLocations = new Location[(int) Main.MapName1.get(Path.MaxTeamCount.toString())];
-    
+
+    public void setSpawnLocations(int spawnNumber, Location location) {
+        spawnLocations[spawnNumber -1] = location;
+    }
 }
